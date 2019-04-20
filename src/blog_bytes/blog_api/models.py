9fc0b5221b45962915398fd795837b8ea0d3ae9b
ritwikjamuar <ritwikjamuar@gmail.com>
2019-04-21 05:09:44 +0530

    Created a model class 'User' for specifying the Table 'User' in DB. Also created a class 'UserManager', which will manage the users in the DB. Thus affected 'blog_api -> models.py'.
    Specified our custom model for marking it as Authorized User Model, thus affected 'blog_bytes -> settings.py'.
    
    Made migrations from our custom model and Django created Migration file under 'blog_api -> migrations'.

--- src/blog_bytes/blog_api/models.py
+++ src/blog_bytes/blog_api/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 # Create your models here.
+
+"""Helps Django to create User with our implementation of managing User"""
+class UserManager ( BaseUserManager ) :
+
+	"""Creates a new User in the System"""
+	def create_user ( self, email, name, password = None ) :
+		# Check whether new User request contains Email Address or not.
+		# This is necessary since 'email' will be used as the Primary Key in our DB.
+		if not email :
+			raise ValueError ( "User must have Email Address" )
+
+		# Normalize the Email Address so that upon entering Email Address with case insensitive,
+		# it will be formatted to same format.
+		email = self.normalize_email ( email )
+
+		# Create an instance of User.
+		user = self.model ( email = email, name = name )
+
+		# Store the Password in Encrypted Form so that password is not clearly stored in DB.
+		user.set_password ( password )
+
+		# Save the User to DB.
+		user.save ( using = self.db )
+		return user
+
+	"""Creates Super User in the System"""
+	def create_super_user ( self, name, email, password ) :
+		# Create a normal user.
+		user = self.create_user ( email, name, password )
+
+		# Specify this user that it is a Super User.
+		user.is_superuser = True
+
+		# Specify this user that it is a Staff.
+		user.staff = True
+
+		# Return the User.
+		return user
+
+"""Represent a User in our System"""
+class User ( AbstractBaseUser, PermissionsMixin ) :
+	email = models.EmailField ( max_length = 255, unique = True )
+	name = models.CharField ( max_length = 50 )
+	active = models.BooleanField ( default = True )
+	staff = models.BooleanField ( default = False )
+
+	objects = UserManager ()
+
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = [ 'name' ]
+
+	"""Gets the Name of User"""
+	def get_name ( self ) :
+		return self.name
+
+	"""Gets the Email of User"""
+	def get_email ( self ) :
+		return self.email
+
+	def __str__ ( self ) :
+		return self.get_email ()
